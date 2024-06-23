c1c939f21cbbbc4c34eca8d7848bccf0ed6617ca
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2024-06-23 16:51:53 +0200

    added basic routing

--- src/components/service/UserService.js
+++ src/components/service/UserService.js
@@ -70,7 +70,7 @@ class UserService
 
     static async updateUser(userId, userData, token) {
         try {
-            const response = await axios.put(`${UserService.BASE_URL}/admin/update/${userId}`, userData, {
+            const response = await axios.put(`${UserService.BASE_URL}/admin/update-user/${userId}`, userData, {
                 headers: {Authorization: `Bearer ${token}`}
             })
             return response.data;
